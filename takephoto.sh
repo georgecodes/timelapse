eb43cc7aa9983f308dc0dc7f5054cba9b1cb5d5e
George McIntosh <george@georgemcintosh.com>
2015-04-26 10:32:24 +0000

    baseline

---
+++ takephoto.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export DIR=`dirname $0`
+export TIMESTAMP=`date +%Y-%m-%d-%H.%M.%S`
+export FILENAME="${DIR}/${TIMESTAMP}.jpg"
+
+gphoto2 --capture-image-and-download --filename ${FILENAME}
+
+if [ $? -ne 0 ]; then 
+  echo "I tried to take a photo ${TIMESTAMP} but it failed for some reason" | mail -s "Photo failed" george@georgemcintosh.com
+  exit -1
+fi
+
+scp ${FILENAME} root@emohawk.local:/volume1/westquay/images/raw/
+
+if [ $? -eq 0 ]; then
+  echo "I took a photo ${TIMESTAMP} and uploaded it" | mail -s "Photo uploaded" george@georgemcintosh.com
+  rm ${FILENAME}
+else
+  echo "I took a photo ${TIMESTAMP} but it didn't upload" | mail -s "Photo taken but not uploaded" george@georgemcintosh.com
+  exit -2
+fi
