b00d4eb4b54ff70b9e7131c69dab96276311df69
George McIntosh <george@georgecodes.com>
2015-04-26 12:04:00 +0100

    logging

--- takephoto.sh
+++ takephoto.sh
@@ -1,29 +1,40 @@
 #!/bin/sh
 
-export DIR="/tmp/photos"
+export DIR="/tmp"
+export LOG_FILE="/var/log/takephoto.log"
 export TIMESTAMP=`date +%Y-%m-%d-%H.%M.%S`
 export FILENAME="${DIR}/${TIMESTAMP}.jpg"
 
-mkdir -p $DIR
-echo "Attempting to take photograph and save in ${FILENAME}"
+function log() {
+	echo $1
+	echo $1 >> $LOG_FILE
+}
+
+log "Attempting to take photograph and save in ${FILENAME}"
 
 OUTPUT=$(gphoto2 --capture-image-and-download --filename ${FILENAME})
 
-echo $OUTPUT
+log $OUTPUT
 
 if [ $? -ne 0 ]; then 
-  echo "I tried to take a photo ${TIMESTAMP} but it failed for some reason\n\n${OUTPUT}" | mail -s "Photo failed" george@georgemcintosh.com
+  FAIL_MESSAGE="I tried to take a photo ${TIMESTAMP} but it failed for some reason\n\n${OUTPUT}"
+  log $FAIL_MESSAGE
+  echo $FAIL_MESSAGE | mail -s "Photo failed" george@georgemcintosh.com
   exit -1
 fi
 
 OUTPUT=$(scp ${FILENAME} root@emohawk.local:/volume1/westquay/images/raw/)
 
-echo $OUTPUT
+log $OUTPUT
 
 if [ $? -eq 0 ]; then
-  echo "I took a photo ${TIMESTAMP} and uploaded it" | mail -s "Photo uploaded" george@georgemcintosh.com
+  MESSAGE="I took a photo ${TIMESTAMP} and uploaded it"
+  log $MESSAGE
+  echo $MESSAGE | mail -s "Photo uploaded" george@georgemcintosh.com
   rm ${FILENAME}
 else
-  echo "I took a photo ${TIMESTAMP} but it didn't upload\n\n${OUTPUT}" | mail -s "Photo taken but not uploaded" george@georgemcintosh.com
+  MESSAGE="I took a photo ${TIMESTAMP} but it didn't upload\n\n${OUTPUT}" 
+  log $MESSAGE
+  echo $MESSAGE | mail -s "Photo taken but not uploaded" george@georgemcintosh.com
   exit -2
 fi
